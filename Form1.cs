a0a84801b395144e117b9777bfc7c8b26919124b
De Wet Kirsten <91884659+Fysobit@users.noreply.github.com>
2021-10-04 17:14:00 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,18 +21,19 @@ namespace Activity
         
         public string checkConnection()
         {
+            string content = "Username: 007, Name: James Bond, Occupation: Assassin\nUsername: Stig, Name: The Stig, Occupation: Reckless Driver\nUsername: Beast, Name: Tendai Mtawarira, Occupation: Lock\nUsername: Speed Merchant, Name: Percy Tau, Occupation: Goalscorer";
+
             if (!File.Exists(path))
-                File.CreateText(path);
-                string content = "Username: 007, Name: James Bond, Occupation: Assassin\nUsername: Stig, Name: The Stig, Occupation: Reckless Driver\nUsername: Beast, Name: Tendai Mtawarira, Occupation: Lock\nUsername: Speed Merchant, Name: Percy Tau, Occupation: Goalscorer";
-                using (StreamWriter sr = File.AppendText(path))
+            {
+                using (StreamWriter sr = File.CreateText(path))
                 {
                     sr.WriteLine(content);
                 }
+            }
             else
                 return "Already exist";
             return path + " is created";
         }
-        
         public string readFile()
         {
             string s;
