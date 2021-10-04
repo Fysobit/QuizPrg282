a3ee2b8883cd8afd34b5e94d14528a5b47431fef
De Wet Kirsten <91884659+Fysobit@users.noreply.github.com>
2021-10-04 15:17:54 +0200

    Reading File

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Activity
 {
@@ -15,6 +16,28 @@ namespace Activity
         public Form1()
         {
             InitializeComponent();
+        }
+        static string path = "Untitled.txt";
+        
+        public string readFile()
+        {
+            string s;
+            string ss=" ";
+            
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while ((s = sr.ReadLine()) != null)
+                {
+                    ss += s;
+                    listBox1.Items.Add(s);
+                }
+            }
+            return ss;
+        }
+        
+        private void btnReadFile_Click(object sender, EventArgs e)
+        {
+            readFile();
         }
     }
 }
